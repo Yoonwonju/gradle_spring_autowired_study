41440645be62455e057e8d6cdff9c7b2955332c6
yoon <lrzrlove@naver.com>
2020-09-23 09:38:18 +0900

    초기 작업 / ignore 까지 수정

---
+++ src/main/java/gradle_spring_autowired_study/spring/MemberNotFoundException.java
@@ -0,0 +1,8 @@
+package gradle_spring_autowired_study.spring;
+
+public class MemberNotFoundException extends RuntimeException {
+	
+//	public MemberNotFoundException(String message) {
+//		super(message);
+//	}
+}
---
+++ src/main/java/gradle_spring_autowired_study/spring/WrongIdPasswordException.java
@@ -0,0 +1,8 @@
+package gradle_spring_autowired_study.spring;
+
+public class WrongIdPasswordException extends RuntimeException {
+	
+//	public WrongIdPasswordException(String message) {
+//		super(message);
+//	}
+}
